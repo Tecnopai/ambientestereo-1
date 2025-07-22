87291258f1a9a326d836a15c5e9cbda18d69e07f
miltonbass <miltonbass@gmail.com>
2025-07-22 14:49:31 -0500

    Archivos

--- lib/views/player/config.dart
+++ lib/views/player/config.dart
@@ -6,7 +6,7 @@
 class Config {
   static const title = 'Radio Ambiente Stereo 88.4FM';
   static const description = 'Playing the Music You Love';
-  static const streamUrl = 'https://radio06.cehis.net:9036/stream';
+  static const streamUrl = 'https://liveingesta118.cdnmedia.tv/unbosquelive/smil:stream1.smil/playlist.m3u8';
 
   // Automatically start playing when the app is launched.
   static const autoplay = true;
